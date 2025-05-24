84db4f8793eedfdf5c64f7919fbdfdf3957e25f8
yosrelrefaai <yousser00@gmial.com>
2025-05-24 22:57:32 +0300

    new-udate2s

---
+++ src/ProtectedRoute.js
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "./usercontext";
+const ProtectedRoute = ({ children }) => {
+  const { currentUser } = useAuth();
+
+  if (!currentUser) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
