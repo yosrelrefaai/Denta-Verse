63d63f6ed498e1c66e4706774609dcf4f5bab233
yosrelrefaai <yousser00@gmial.com>
2025-05-26 16:40:33 +0300

    responsive 5e

--- src/SecondSection.js
+++ src/SecondSection.js
@@ -20,7 +20,8 @@ export default function SecondSection(){
          <button className='bg-gradient-to-r from-teal-custom-Poppy to-teal-custom-orange rounded-full text-white text-base p-3 font-semibold transition-all duration-500 tranform hover:scale-105'> اعرف أكتر </button>
          </Link>
        </div>
-       <div className=" h-auto w-full md:max-w-lg flex-1 border-teal-custom-orange border-2 bg-white/60 rounded-lg  md:rounded-lg p-3 flex flex-col justify-center items-center gap-3 transition-all duration-500 hover:scale-105 hover:shadow-md">
+       <div className=" h-auto w-[90%] mx-auto md:max-w-lg border-teal-custom-orange border-2 bg-white/60 rounded-lg p-3 flex flex-col justify-center items-center gap-3 transition-all duration-500 hover:scale-105 hover:shadow-md
+">
        <LazyLoadImage src={weakteeth} alt="weak teeth" className="w-36 h-44 object-cover rounded-md"/>
          <h3 className="text-teal-custom-orange  text-center text-lg font-semibold px-5  py-2"> لأسنان ضعيفة و مسوسة </h3>
          <p className="text-gray-700 sm:text-sm text-xs text-center">تفتكر ايه ممكن يخلي أسنانك ضعيفة و مسوسة </p>
